f29a1640301bf8e2cf5b39da4c740d6d4bf4d02a
Manav Jawrani <manavjawrani7@gmail.com>
2023-03-19 16:56:06 +0530

    Update

--- app.py
+++ app.py
@@ -15,11 +15,7 @@ from tensorflow.keras.applications.vgg16 import preprocess_input
 
 # Loading Models
 covid_model = load_model('models/covid.h5')
-braintumor_model = load_model('models/braintumor.h5')
-alzheimer_model = load_model('models/alzheimer_model.h5')
-diabetes_model = pickle.load(open('models/diabetes.sav', 'rb'))
 heart_model = pickle.load(open('models/heart_disease.pickle.dat', "rb"))
-pneumonia_model = load_model('models/pneumonia_model.h5')
 breastcancer_model = joblib.load('models/cancer_model.pkl')
 
 # Configuring Flask
@@ -96,27 +92,6 @@ def covid():
 def breast_cancer():
     return render_template('breastcancer.html')
 
-
-@app.route('/braintumor')
-def brain_tumor():
-    return render_template('braintumor.html')
-
-
-@app.route('/diabetes')
-def diabetes():
-    return render_template('diabetes.html')
-
-
-@app.route('/alzheimer')
-def alzheimer():
-    return render_template('alzheimer.html')
-
-
-@app.route('/pneumonia')
-def pneumonia():
-    return render_template('pneumonia.html')
-
-
 @app.route('/heartdisease')
 def heartdisease():
     return render_template('heartdisease.html')
@@ -156,59 +131,6 @@ def resultc():
             return redirect(request.url)
 
 
-@app.route('/resultbt', methods=['POST'])
-def resultbt():
-    if request.method == 'POST':
-        firstname = request.form['firstname']
-        lastname = request.form['lastname']
-        email = request.form['email']
-        phone = request.form['phone']
-        gender = request.form['gender']
-        age = request.form['age']
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            flash('Image successfully uploaded and displayed below')
-            img = cv2.imread('static/uploads/'+filename)
-            img = crop_imgs([img])
-            img = img.reshape(img.shape[1:])
-            img = preprocess_imgs([img], (224, 224))
-            pred = braintumor_model.predict(img)
-            if pred < 0.5:
-                pred = 0
-            else:
-                pred = 1
-            # pb.push_sms(pb.devices[0],str(phone), 'Hello {},\nYour Brain Tumor test results are ready.\nRESULT: {}'.format(firstname,['NEGATIVE','POSITIVE'][pred]))
-            return render_template('resultbt.html', filename=filename, fn=firstname, ln=lastname, age=age, r=pred, gender=gender)
-
-        else:
-            flash('Allowed image types are - png, jpg, jpeg')
-            return redirect(request.url)
-
-
-@app.route('/resultd', methods=['POST'])
-def resultd():
-    if request.method == 'POST':
-        firstname = request.form['firstname']
-        lastname = request.form['lastname']
-        email = request.form['email']
-        phone = request.form['phone']
-        gender = request.form['gender']
-        pregnancies = request.form['pregnancies']
-        glucose = request.form['glucose']
-        bloodpressure = request.form['bloodpressure']
-        insulin = request.form['insulin']
-        bmi = request.form['bmi']
-        diabetespedigree = request.form['diabetespedigree']
-        age = request.form['age']
-        skinthickness = request.form['skin']
-        pred = diabetes_model.predict(
-            [[pregnancies, glucose, bloodpressure, skinthickness, insulin, bmi, diabetespedigree, age]])
-        # pb.push_sms(pb.devices[0],str(phone), 'Hello {},\nYour Diabetes test results are ready.\nRESULT: {}'.format(firstname,['NEGATIVE','POSITIVE'][pred]))
-        return render_template('resultd.html', fn=firstname, ln=lastname, age=age, r=pred, gender=gender)
-
-
 @app.route('/resultbc', methods=['POST'])
 def resultbc():
     if request.method == 'POST':
@@ -227,67 +149,6 @@ def resultbc():
             np.array([cpm, am, rm, pm, cm]).reshape(1, -1))
         # pb.push_sms(pb.devices[0],str(phone), 'Hello {},\nYour Breast Cancer test results are ready.\nRESULT: {}'.format(firstname,['NEGATIVE','POSITIVE'][pred]))
         return render_template('resultbc.html', fn=firstname, ln=lastname, age=age, r=pred, gender=gender)
-
-
-@app.route('/resulta', methods=['GET', 'POST'])
-def resulta():
-    if request.method == 'POST':
-        print(request.url)
-        firstname = request.form['firstname']
-        lastname = request.form['lastname']
-        email = request.form['email']
-        phone = request.form['phone']
-        gender = request.form['gender']
-        age = request.form['age']
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            flash('Image successfully uploaded and displayed below')
-            img = cv2.imread('static/uploads/'+filename)
-            img = cv2.resize(img, (176, 176))
-            img = img.reshape(1, 176, 176, 3)
-            img = img/255.0
-            pred = alzheimer_model.predict(img)
-            pred = pred[0].argmax()
-            print(pred)
-            # pb.push_sms(pb.devices[0],str(phone), 'Hello {},\nYour Alzheimer test results are ready.\nRESULT: {}'.format(firstname,['NonDemented','VeryMildDemented','MildDemented','ModerateDemented'][pred]))
-            return render_template('resulta.html', filename=filename, fn=firstname, ln=lastname, age=age, r=0, gender=gender)
-
-        else:
-            flash('Allowed image types are - png, jpg, jpeg')
-            return redirect('/')
-
-
-@app.route('/resultp', methods=['POST'])
-def resultp():
-    if request.method == 'POST':
-        firstname = request.form['firstname']
-        lastname = request.form['lastname']
-        email = request.form['email']
-        phone = request.form['phone']
-        gender = request.form['gender']
-        age = request.form['age']
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            flash('Image successfully uploaded and displayed below')
-            img = cv2.imread('static/uploads/'+filename)
-            img = cv2.resize(img, (150, 150))
-            img = img.reshape(1, 150, 150, 3)
-            img = img/255.0
-            pred = pneumonia_model.predict(img)
-            if pred < 0.5:
-                pred = 0
-            else:
-                pred = 1
-            # pb.push_sms(pb.devices[0],str(phone), 'Hello {},\nYour COVID-19 test results are ready.\nRESULT: {}'.format(firstname,['POSITIVE','NEGATIVE'][pred]))
-            return render_template('resultp.html', filename=filename, fn=firstname, ln=lastname, age=age, r=pred, gender=gender)
-
-        else:
-            flash('Allowed image types are - png, jpg, jpeg')
-            return redirect(request.url)
 
 
 @app.route('/resulth', methods=['POST'])
